a4ec52f98e783457457f6351d085769f6ae48a80
Ghalia <rebazaghalia27@gmail.com>
2021-05-08 00:57:48 -0500

    primer commit

---
+++ src/main/java/org/gaming/ProyectoGamingAddictedApplication.java
@@ -0,0 +1,13 @@
+package org.gaming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoGamingAddictedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoGamingAddictedApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/gaming/ProyectoGamingAddictedApplicationTests.java
@@ -0,0 +1,13 @@
+package org.gaming;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoGamingAddictedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
