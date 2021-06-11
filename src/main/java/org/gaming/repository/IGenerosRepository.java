c4f3893c273f41da8acb66883451fc243042eb69
jose-asencios <84046359+jose-asencios@users.noreply.github.com>
2021-06-10 23:24:05 -0500

    Add files via upload

---
+++ src/main/java/org/gaming/repository/IGenerosRepository.java
@@ -0,0 +1,8 @@
+package org.gamingaddicted.repository;
+
+import org.gamingaddicted.model.Generos;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface IGenerosRepository extends JpaRepository<Generos, Integer>{
+
+}
---
+++ src/main/java/org/gaming/repository/IPerfilesRepository.java
@@ -0,0 +1,8 @@
+package org.gamingaddicted.repository;
+
+import org.gamingaddicted.model.Perfiles;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface IPerfilesRepository extends JpaRepository<Perfiles, Integer>{
+
+}
