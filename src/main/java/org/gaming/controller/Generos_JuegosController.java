8b4b2fcb90128d90229a7e15a779254ee46dc5d5
Dennis040119 <83846957+Dennis040119@users.noreply.github.com>
2021-06-18 17:23:40 -0500

    Add files via upload

--- src/main/java/org/gaming/controller/Generos_JuegosController.java
+++ src/main/java/org/gaming/controller/Generos_JuegosController.java
@@ -1,11 +1,18 @@
 package org.gaming.controller;
 
+
+
+import org.gaming.model.Generos_juegos;
+import org.gaming.model.Perfiles;
 import org.gaming.repository.IGeneros_JuegosRepository;
-import org.gaming.repository.IGeneros_JuegosRepository;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class Generos_JuegosController {
@@ -24,10 +31,44 @@ public class Generos_JuegosController {
 		return "List_GenJuegos";
 	}
 	
-	@GetMapping("/RgstGenJuegos")
-	public String RgstGenJuegos(Model model) {
-
-		
+	
+	@PostMapping("/RgstGenJuegos")
+	public String guardarPerfil(@ModelAttribute Generos_juegos GenJuego, Model model) throws Exception{	
+		System.out.println(GenJuego);
+		String message = null;
+		ModelAndView vista = new ModelAndView();
+		try {
+			repo.save(GenJuego);
+			System.out.println("BIEN.X2......");
+			//model.addAttribute("message","Baeldung");
+			vista.setViewName("Registrar_GenJuegos");
+			vista.addObject("mensaje", "Insertado Correctamente");
+		}catch(Exception e) {
+			System.out.println("ERROR.......");
+			
+		};
+		model.addAttribute("GenJuego", repo.findById(GenJuego.getIdgenero()));
 		return "Registrar_GenJuegos";
 	}
+	
+	@PostMapping("/buscarGenJue")
+	public String buscarGen(@ModelAttribute Generos_juegos  gj, Model model) {
+		
+		System.out.println(gj);
+		model.addAttribute("GenJuego", repo.findById(gj.getIdgenero()));
+		model.addAttribute("opcion", 1);
+		return "Registrar_GenJuegos";
+		
+	
+	}
+	@GetMapping("/eliminarGenJue")
+	public String eliminarPerfil(@ModelAttribute Generos_juegos p, Model model) {
+		System.out.println(p);
+		repo.delete(p);
+		model.addAttribute("GenJuego",repo.findById(p.getIdgenero()));
+		model.addAttribute("lstGenJuegos", repo.findAll());
+		return "List_GenJuegos";
+	}
+	
+	
 }
--- src/main/java/org/gaming/controller/JuegosController.java
+++ src/main/java/org/gaming/controller/JuegosController.java
@@ -1,34 +1,89 @@
 package org.gaming.controller;
 
+
+import org.gaming.model.Generos_juegos;
+import org.gaming.model.Juegos;
+import org.gaming.repository.IClasificacionRepository;
+import org.gaming.repository.IEdicionRepository;
 import org.gaming.repository.IGeneros_JuegosRepository;
 import org.gaming.repository.IJuegosRepository;
+import org.gaming.repository.IPlataformaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class JuegosController {
 
 	@Autowired
-	private IJuegosRepository repo;
+	private IJuegosRepository repoJuegos;
+	@Autowired
+	private IGeneros_JuegosRepository repoGenJuegos;
+	@Autowired
+	private IEdicionRepository repoEdicion;
+	@Autowired
+	private IClasificacionRepository repoClasificacion;
+	@Autowired
+	private IPlataformaRepository repoPlataforma;
+	
+	
 	
 	@GetMapping("/listJuegos")
 	public String listadoGenJuegos(Model model) {
 		
 		
-		model.addAttribute("lstJuegos", repo.findAll());
+		model.addAttribute("lstJuegos", repoJuegos.findAll());
 		
 		
 		
 		return "List_Juegos";
 	}
 	
-	@GetMapping("/RgstJuegos")
-	public String RgstJuegos(Model model) {
-
-		
+	@PostMapping("/RgstJuegos")
+	public String guardarPerfil(@ModelAttribute Juegos Juego, Model model) throws Exception{	
+		System.out.println(Juego);
+		String message = null;
+		ModelAndView vista = new ModelAndView();
+		try {
+			repoJuegos.save(Juego);
+			System.out.println("BIEN.X2......");
+			
+			vista.setViewName("Registrar_Juegos");
+			vista.addObject("mensaje", "Insertado Correctamente");
+		}catch(Exception e) {
+			System.out.println("ERROR.......");
+			
+		};
+		model.addAttribute("Juego", repoJuegos.findById(Juego.getIdjuego()));
 		return "Registrar_Juegos";
+	}
+	@PostMapping("/buscarJuego")
+	public String buscarJuego(@ModelAttribute Juegos  gj, Model model) {
+		
+		System.out.println(gj);
+		model.addAttribute("ListGenJuego", repoGenJuegos.findAll());
+		model.addAttribute("ListEdicion", repoEdicion.findAll());
+		model.addAttribute("ListPlataform", repoPlataforma.findAll());
+		model.addAttribute("ListClasifi", repoClasificacion.findAll());
+		model.addAttribute("Juego", repoJuegos.findById(gj.getIdgenero()));
+		int a =repoJuegos.findAll().size()+1;
+		model.addAttribute("id", a);
+		model.addAttribute("opcion", 1);
+		return "Registrar_Juegos";
+		
+	
+	}
+	@GetMapping("/eliminarJue")
+	public String eliminarPerfil(@ModelAttribute Juegos p, Model model) {
+		System.out.println(p);
+		repoJuegos.delete(p);
+		model.addAttribute("Juego",repoJuegos.findById(p.getIdgenero()));
+		model.addAttribute("lstJuegos", repoJuegos.findAll());
+		return "List_Juegos";
 	}
 	
 }
