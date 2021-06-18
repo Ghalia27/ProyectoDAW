8075ccdb2fac5ed6a110bda91627232b68f43926
Dennis040119 <83846957+Dennis040119@users.noreply.github.com>
2021-06-18 17:25:37 -0500

    Add files via upload

---
+++ src/main/java/org/gaming/repository/IEdicionRepository.java
@@ -0,0 +1,8 @@
+package org.gaming.repository;
+
+import org.gaming.model.Ediciones;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface IEdicionRepository extends JpaRepository<Ediciones, Integer> {
+
+}
---
+++ src/main/java/org/gaming/repository/IPlataformaRepository.java
@@ -0,0 +1,11 @@
+package org.gaming.repository;
+
+import org.gaming.model.Generos;
+import org.gaming.model.Plataformas;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface IPlataformaRepository extends JpaRepository<Plataformas, Integer> {
+	
+
+
+}
